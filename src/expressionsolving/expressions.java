96706553e5a6abf5e4df4ee51e9342438b23e335
mutyalashailaja <mutyala.shailaja@gmail.com>
2016-08-29 17:03:06 +0530

    session1ass4

---
+++ src/expressionsolving/expressions.java
@@ -0,0 +1,24 @@
+package expressionsolving;
+
+public class expressions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=5;
+		int y=10;
+		int a,b,c,d;
+		 a=x + y * 2;
+		 b= x - y + 2;
+		 System.out.println(b);
+		 c= (x+y) * 2;
+		 d= y % x;
+
+		 System.out.println(a);
+		 
+		 System.out.println(c);
+		 System.out.println(d);
+		 
+
+	}
+
+}
